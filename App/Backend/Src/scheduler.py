3d722dd77eaab6b36aa3041d74942f5c9fa5d05d
dagim <dagimgenene1@gmail.com>
2025-03-06 21:09:09 +0300

    Optimized calculate in schedule to add dates without me doing it manually

--- App/Backend/Src/scheduler.py
+++ App/Backend/Src/scheduler.py
@@ -3,6 +3,7 @@
 """Schedules the next day to review a topic."""
 import math
 import sqlite3
+from datetime import datetime ,timedelta
 
 from initentity import InitEntity
 import config
@@ -10,18 +11,22 @@ import config
 
 class Scheduler(InitEntity):
     """A class for scheduling the next review day of a subject"""
-    def calculate(self):
-       unscheduled_topics = self.connection.execute("SELECT topic_id FROM topics WHERE NOT EXISTS (SELECT 1 FROM reviews_box WHERE reviews_box.topic_id = topics.topic_id )").fetchall()
-       # for each unschdule id,schedule them.
-       for topic_id in unscheduled_topics:
-           last_seen = self.connection.execute("SELECT last_seen FROM topics WHERE topic_id = ?",topic_id).fetchall()
-           
-           rating = self.connection.execute("SELECT rating FROM topics WHERE topic_id = ?",topic_id).fetchall()
-           rating = int(rating[0][0])
-           
-           last_seen = last_seen[0][0].split("-")
-           next_review = int(last_seen[-1]) + round(math.e**(0.6*rating))
-           return next_review
+    def calculate(self) -> list[tuple[int,str]]:
+        """Returns a list that contain tuple of topic_id and next review day."""
+        query = "SELECT topics.topic_id, topics.last_seen, topics.rating FROM topics WHERE NOT EXISTS (SELECT 1 FROM reviews_box WHERE reviews_box.topic_id = topics.topic_id)"
+        unscheduled_topics = self.connection.execute(query).fetchall()
+        # for each unschdule id,schedule them.
+        next_review_day = []
+        for topic_id,last_seen , rating in unscheduled_topics:
+            last_seen = datetime.strptime(last_seen,'%Y-%m-%d')# change the string day into datetime object
+
+            review_interval = round(math.e**(0.6*int(rating)))
+
+            next_review = timedelta(round(math.e**(0.6*rating)))
+            next_review_date = last_seen + next_review
+            next_review_day.append((topic_id,next_review_date.strftime('%Y-%m-%d')))
+
+            return next_review_day
 
 s = Scheduler(config.db_path)
 print(s.calculate())
