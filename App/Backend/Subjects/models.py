072ae0e00a320bcdffe6da93cbc436ad061c5b6a
dagim <dagimgenene1@gmail.com>
2025-03-28 22:55:09 +0300

    admin site is created

--- App/Backend/Subjects/models.py
+++ App/Backend/Subjects/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 class Subjects(models.Model):
@@ -14,3 +16,13 @@ class Topics(models.Model):
     
     def __str__(self):
         return self.topic_name
+
+class Subtopics(models.Model):
+    topic = models.ForeignKey(Topics,on_delete=models.CASCADE)
+    subtopic_name = models.CharField(max_length=500)
+    rating = models.IntegerField(default=3)
+    last_seen = models.DateTimeField(date.today().isoformat())
+
+    def __str__(self):
+        return self.subtopic_name
+
