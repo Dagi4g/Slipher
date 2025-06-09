17a0638e8c3edfb3758a3f9d43ac3fb18abb7d5f
Dagi4g <dagimgenene1@gmail.com>
2025-06-09 11:20:51 +0300

    the topic class based view is working

--- App/Backend/Subjects/views0/topic.py
+++ App/Backend/Subjects/views0/topic.py
@@ -5,10 +5,12 @@ from Subjects import models,forms
 class TopicListView(ListView):
     model = models.Topics
     template_name = "Subjects/topic/topic.html"
+    context_object_name = "topic_list"
 
     def get_queryset(self):
         subject = get_object_or_404(models.Subjects,id=self.kwargs["subject_id"])
-        return subject.topic.all()
+        topic_list = subject.topic.all()
+        return topic_list
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
